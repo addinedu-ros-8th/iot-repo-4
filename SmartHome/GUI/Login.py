8d21d95af0e73befa3f2d77517d051c068c8ba9c
YuSoYu <hey6410@gmail.com>
2025-02-16 22:18:10 +0900

    Login.py Edit

--- SmartHome/GUI/Login.py
+++ SmartHome/GUI/Login.py
@@ -1,60 +1,41 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 from PyQt5 import uic
-<<<<<<< HEAD
 import mysql.connector
+from Main import WindowClass  # Main.py의 WindowClass 가져오기
 
 # UI 파일 로드
 LoginUI = uic.loadUiType("./GUI/Login.ui")[0]
-MainUI = uic.loadUiType("./GUI/Main.ui")[0]
-
-class MainWindow(QMainWindow, MainUI):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)  # Main.ui 로드
-=======
-from Main import WindowClass  # ✅ Main.py의 WindowClass 가져오기
-
-# UI 파일 로드
-LoginUI = uic.loadUiType("Login.ui")[0]
->>>>>>> 3c714bc (ON_OFF_toggle slider)
 
 class LoginWindow(QMainWindow, LoginUI):
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # Login.ui 로드
         self.remote = mysql.connector.connect(
-            host = "database-1.c7iiuw4kenou.ap-northeast-2.rds.amazonaws.com",
-            user = "chillHome",
-            password = "addinedu1!",
-            database = "chillHome"
+            host="database-1.c7iiuw4kenou.ap-northeast-2.rds.amazonaws.com",
+            user="chillHome",
+            password="addinedu1!",
+            database="chillHome"
         )
 
         # 로그인 버튼 클릭 시 Main.py의 WindowClass 실행
         self.Login_btn.clicked.connect(self.open_main_window)
 
-
     def open_main_window(self):
-<<<<<<< HEAD
+        """ MySQL 로그인 검증 후 Main.py 실행 """
         id = self.lineEdit.text()
         passwd = self.lineEdit_2.text()
-        
+
         cursor = self.remote.cursor()
         cursor.execute(f"SELECT COUNT(*) as cnt FROM users WHERE id = '{id}' and password = '{passwd}'")
-
         result = cursor.fetchone()
-        
+
         if result[0] > 0:
-            self.main_window = MainWindow()  # MainWindow 인스턴스 생성
-            self.main_window.show()  # 메인 윈도우 표시
+            self.main_window = WindowClass()  # Main.py의 WindowClass 실행
+            self.main_window.show()  # Main.ui를 실행
             self.close()  # 현재 로그인 창 닫기
         else:
-            QMessageBox.warning(self, 'QMessageBox - set title', '아이디 또는 비밀번호가 틀렸습니다. 확인 후 다시 시도해주세요.')
-=======
-        self.main_window = WindowClass()  # ✅ Main.py의 WindowClass 실행
-        self.main_window.show()  # ✅ Main.ui를 실행
-        self.close()  # 현재 로그인 창 닫기
->>>>>>> 3c714bc (ON_OFF_toggle slider)
+            QMessageBox.warning(self, '로그인 실패', '아이디 또는 비밀번호가 틀렸습니다. 확인 후 다시 시도해주세요.')
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
