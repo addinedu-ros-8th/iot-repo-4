31de98174f7f915bab557649e1d5f35cd93eb8d1
LeeSangYun <sangyun97@gmail.com>
2025-02-26 02:03:05 +0900

    garageDoorControll DONE

--- SmartHome/garageDoorController/garageCameraWebServer/garageCameraWebServer.ino
+++ SmartHome/garageDoorController/garageCameraWebServer/garageCameraWebServer.ino
@@ -36,8 +36,8 @@
 // ===========================
 // Enter your WiFi credentials
 // ===========================
-const char *ssid = "AIE_509_2.4G";
-const char *password = "addinedu_class1";
+const char *ssid = "torry";
+const char *password = "1608008993";
 
 void startCameraServer();
 void setupLedFlash(int pin);
