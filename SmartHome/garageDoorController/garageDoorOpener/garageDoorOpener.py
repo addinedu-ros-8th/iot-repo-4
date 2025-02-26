4d5bdcfb12f38013ef40c9357d58f6ea9dbb5388
sangyun1729 <sangyun97@gmail.com>
2025-02-26 10:23:40 +0900

    garageDoor previous version

--- SmartHome/garageDoorController/garageDoorOpener/garageDoorOpener.py
+++ SmartHome/garageDoorController/garageDoorOpener/garageDoorOpener.py
@@ -6,10 +6,10 @@ import easyocr
 import numpy as np
 import mysql.connector
 
-# arduino = serial.Serial('/dev/ttyUSB0', 9600)
+arduino = serial.Serial('/dev/ttyACM0', 9600)
 
 # ESP32-CAM의 IP 주소
-ESP32_CAM_URL = "http://192.168.0.49/capture"
+ESP32_CAM_URL = "http://192.168.0.13/capture"
 
 # DB 설정
 db = remote = mysql.connector.connect(
