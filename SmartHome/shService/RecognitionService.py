875bf3d9af68afd10011c81315f66a51ebc08cf7
songwonjoon <djckoreas@naver.com>
2025-02-27 18:45:38 +0900

    add fail save image

--- SmartHome/shService/RecognitionService.py
+++ SmartHome/shService/RecognitionService.py
@@ -219,6 +219,8 @@ try:
         if failCount >= 5:
             if lockout_time is None:
                 lockout_time = time.time()  # 현재 시간 저장
+                fileName = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + ".png"
+                cv2.imwrite(fileName, frame)
 
             elapsed_time = time.time() - lockout_time
             remaining_time = LOCKOUT_DURATION - elapsed_time
@@ -284,8 +286,6 @@ try:
                 cv2.putText(frame, "Fail", (f.left()+6,f.bottom()-6), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 225), 2)
                 failCount += 1
                 addFailLogStatus()
-                fileName = datetime.datetime.now().strftime("%Y%m%d_%H%M%S") + ".png"
-                cv2.imwrite(fileName, frame)
                
 
             last_auth_time = time.time()
